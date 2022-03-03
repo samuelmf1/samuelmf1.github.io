bd7353e592f33f1ea5070b19d8d66f6a55b5ad9c
Sam Friedman <smf252@hginj.rutgers.edu>
2022-03-02 23:20:51 -0500

    New features and directory updates

--- apps/sharpmem/sharpmem.js
+++ apps/sharpmem/sharpmem.js
@@ -57,6 +57,27 @@ const attemptsDisplay = document.querySelector('#attempts')
 const scaledScoreDisplay = document.querySelector('#scaled-score')
 const congratsDisplay = document.querySelector('#congrats')
 
+const rulesDisplay = document.querySelector('#rules')
+rulesDisplay.addEventListener("mouseover", showRules, false);
+function showRules() {
+    rulesDisplay.setAttribute("style", "text-decoration: none;")
+    rulesDisplay.textContent = "Select any tile and it will flip over to display an image. Each attempt requires that you flip over two tiles; your goal is always to select two identical tiles. If they do, in fact, match, your score goes up by one point and those tiles both disappear (making it easier to find more matches). If, however, the tiles you selected do not match, they will flip back over so you can make another attempt to match different tiles. Do your best to remember what images are on the tiles you have already interacted with, as it will help you clear the board in fewer attempts. The fewer attempts it takes you to clear the board, the better your performance and the higher your final score will be, because your ratio of successes:attempts will be higher."
+}
+rulesDisplay.addEventListener("mouseout", hideRules, false);
+function hideRules() {
+    rulesDisplay.textContent = "show rules"
+    rulesDisplay.setAttribute("style", "text-decoration: overline;")
+}
+
+const hardModeDisplay = document.querySelector('#detail')
+hardModeDisplay.addEventListener("mouseover", showDetail, false)
+function showDetail() {
+    hardModeDisplay.textContent = " (1/2 the time to memorize tile selections)"
+}
+rulesDisplay.addEventListener("mouseout", hideDetail, false);
+function hideDetail() {
+    hardModeDisplay.textContent = " (?)"
+}
 
 let cardsChosen = []
 let cardsChosenIds = []
@@ -69,7 +90,12 @@ function flipCard() {
     cardsChosenIds.push(cardId)
     this.setAttribute('src', cardArray[cardId].img)
     if (cardsChosen.length === 2) {
-        setTimeout(checkMatch, 480)
+        let timeOutSetting = document.getElementById("toggle").checked
+        if (timeOutSetting) {
+            setTimeout(checkMatch, 300)
+        } else {
+            setTimeout(checkMatch, 600)
+        }
     }
 }
 
@@ -128,6 +154,7 @@ function checkMatch() {
     if (cardsWon.length === (cardArray.length/2)) {
         scaledScoreDisplay.textContent = ""
         congratsDisplay.innerHTML = "You acheived a final score of " + scaledScore + "!"
+        congratsDisplay.setAttribute("style", "color:maroon; ")
         gridDisplay.innerHTML = ""
     }
 }
